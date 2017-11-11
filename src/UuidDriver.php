0b8e82545b256f8442d1c331bb2e9b89b35f491d
Elf Sundae <elf.sundae@gmail.com>
2017-11-12 00:53:31 +0800

    Done UuidDriver

--- src/UuidDriver.php
+++ src/UuidDriver.php
@@ -2,6 +2,80 @@
 
 namespace ElfSundae\Laravel\Hashid;
 
+use Exception;
+use Ramsey\Uuid\Uuid;
+use Illuminate\Support\Arr;
+use InvalidArgumentException;
+
 class UuidDriver implements DriverInterface
 {
+    /**
+     * The hashid manager instance.
+     *
+     * @var \ElfSundae\Laravel\Hashid\HashidManager
+     */
+    protected $manager;
+
+    /**
+     * The hashid connection name.
+     *
+     * @var string
+     */
+    protected $connection;
+
+    /**
+     * Create a new UUID driver instance.
+     *
+     * @param  \ElfSundae\Laravel\Hashid\HashidManager  $manager
+     * @param  array  $config
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct(HashidManager $manager, array $config)
+    {
+        $this->manager = $manager;
+
+        $this->connection = Arr::get($config, 'connection', function () {
+            throw new InvalidArgumentException('A connection name must be specified.');
+        });
+    }
+
+    /**
+     * Encode the data.
+     *
+     * @param  string|\Ramsey\Uuid\Uuid  $data
+     * @return string
+     */
+    public function encode($data)
+    {
+        $uuid = $data instanceof Uuid ? $data : Uuid::fromString($data);
+
+        return $this->getConnection()->encode($uuid->getBytes());
+    }
+
+    /**
+     * Decode the data.
+     *
+     * @param  string  $data
+     * @return \Ramsey\Uuid\Uuid
+     */
+    public function decode($data)
+    {
+        try {
+            return Uuid::fromBytes($this->getConnection()->decode($data));
+        } catch (Exception $e) {
+        }
+
+        return Uuid::fromString(Uuid::NIL);
+    }
+
+    /**
+     * Get the hashid connection instance.
+     *
+     * @return mixed
+     */
+    protected function getConnection()
+    {
+        return $this->manager->connection($this->connection);
+    }
 }
---
+++ tests/UuidDriverTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace ElfSundae\Laravel\Hashid\Test;
+
+use Mockery as m;
+use Ramsey\Uuid\Uuid;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use ElfSundae\Laravel\Hashid\UuidDriver;
+use ElfSundae\Laravel\Hashid\Base62Driver;
+use ElfSundae\Laravel\Hashid\HashidManager;
+
+class UuidDriverTest extends TestCase
+{
+    protected function tearDown()
+    {
+        m::close();
+    }
+
+    public function testInstantiation()
+    {
+        $this->assertInstanceOf(UuidDriver::class, $this->getDriver(['connection' => 'foo']));
+    }
+
+    public function testThrowsExceptionWhenNoConnectionSpecified()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('A connection name must be specified.');
+        $this->getDriver();
+    }
+
+    public function testEncodedAndDecoded()
+    {
+        $driver = $this->getDriver(['connection' => 'foo']);
+        $uuid = Uuid::uuid4();
+
+        $encoded1 = $driver->encode($uuid);
+        $encoded2 = $driver->encode($uuid->toString());
+        $encoded3 = $driver->encode($uuid->getHex());
+        $this->assertSame($encoded1, $encoded2);
+        $this->assertSame($encoded1, $encoded3);
+
+        $decoded = $driver->decode($encoded1);
+        $this->assertTrue($uuid->equals($decoded));
+    }
+
+    public function testFailedDecoded()
+    {
+        $driver = $this->getDriver(['connection' => 'foo']);
+        $decoded = $driver->decode('bar');
+        $this->assertTrue($decoded->equals(Uuid::fromString(Uuid::NIL)));
+    }
+
+    protected function getDriver(array $config = [])
+    {
+        $manager = m::mock(HashidManager::class);
+        $manager->shouldReceive('connection')
+            ->andReturn(new Base62Driver);
+
+        return new UuidDriver($manager, $config);
+    }
+}
